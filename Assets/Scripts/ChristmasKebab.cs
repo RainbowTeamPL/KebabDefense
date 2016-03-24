1a60591a7b530909b3ad0d922e9e0bc11c4414b6
SuperStarPL <f1time@o2.pl>
2016-03-24 14:30:41 +0100

    Christmas Main Menu easter egg

---
+++ Assets/Scripts/ChristmasKebab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ChristmasKebab : MonoBehaviour
+{
+    public AudioClip ChristmasMusic;
+
+    public bool test;
+
+    [HideInInspector]
+    public bool isChristmas { get; private set; }
+
+    // Use this for initialization
+    public void Start()
+    {
+        DateTime date = DateTime.Today;
+        int Day = date.Day;
+        int Month = date.Month;
+        if (Month == 12 || test)
+        {
+            if (Day == 24 || Day == 25 || Day == 26 || test)
+            {
+                isChristmas = true;
+                GetComponent<AudioSource>().clip = ChristmasMusic;
+                GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
